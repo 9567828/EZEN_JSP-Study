b52114c559390a42896c7b14990f0fa7eeff370e
kimssi9567 <kimssi9567@gamil.com>
2024-07-24 17:39:07 +0900

    chap04 login servlet 추가

--- chap04/src/main/java/chap04/servlet/LoginServlet2.java
+++ chap04/src/main/java/chap04/servlet/LoginServlet2.java
@@ -1,7 +1,5 @@
 package chap04.servlet;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,44 +8,45 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/login/*")
-public class LoginServlet extends HttpServlet {
+public class LoginServlet2 extends HttpServlet {
+	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req,
+			HttpServletResponse resp) throws ServletException, IOException {
 		String uri = req.getRequestURI();
-		String cmd = uri.substring("/chap04/login".length());
+		String cmd = uri.substring("/chap04/login/".length());
+		
 		
 		System.out.println("ip addr: " + req.getRemoteAddr());
-		System.out.println("URL: " + req.getRequestURL());
-		System.out.println("URI: " + uri);
-		System.out.println("CMD: " + cmd);
+		System.out.println("url: " + req.getRequestURL());
+		System.out.println("uri: " + uri);
+		System.out.println("cmd: " + cmd);
 		
 		HttpSession session = req.getSession();
 		
 		if (cmd.equals("success")) {
 			session.setAttribute("user", "김민수");
 			
-			// 해당 세션ID에 만료시간 설정하기
+			// 해당 세션ID에 만료시간 설정하기 (초)
 			session.setMaxInactiveInterval(10);
 			
 			resp.sendRedirect("/chap04/login/result");
 		} else if (cmd.equals("result")) {
-			// 포워드 걸 때는 webapp 밑에 경로를 건다
 			req.getRequestDispatcher("/session/session_ex.jsp").forward(req, resp);
 		} else if (cmd.equals("logout")) {
-			// 세션 어트리뷰트에 user가 없으면 로그아웃으로 간주
+			// 세션 어트리뷰트에 user 가 없으면 로그아웃으로 간주
 			session.removeAttribute("user");
-		
-			// 로그아웃시 세션 만료시간 다시 30분으로 설정하기
+			
 			session.setMaxInactiveInterval(1800); // 세션 만료시간 없애기
 			
 			resp.sendRedirect("/chap04/login/result");
-		} else if (cmd.equals("close")) {
-			// 세션만료시키기
-			session.invalidate();
 			
-			// 리다이렉트시 바로 새로운 세션ID를 발급받게 된다
+		} else if (cmd.equals("close")) {
+			session.invalidate(); // 세션 만료시키기
+			
+			// 세션 만료 후 리다이렉트시 바로 새로운 세션ID를 발급받게 된다
 			resp.sendRedirect("/chap04/login/result");
+			
 		}
-		
 	}
 }
