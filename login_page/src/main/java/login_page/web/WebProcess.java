8df8094bb977c7a37838d279b9c6a913012bc136
김시은 <73893978+9567828@users.noreply.github.com>
2024-08-01 04:34:27 +0900

    Delete login_page directory

--- login_page/src/main/java/login_page/web/WebProcess.java
+++
@@ -1,9 +0,0 @@
-package login_page.web;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface WebProcess {
-
-	String process(HttpServletRequest request, HttpServletResponse response);
-}
